06cd699b4f8e97d4267d8d7269ddceeb1f6de777
wangechimk <wangechik.michelle@gmail.com>
2021-04-28 19:19:09 +0300

    add imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+app.static_folder = 'static'
+db = SQLAlchemy()
+bootstrap = Bootstrap()
 
 
-def create_app():
+def create_app(config_name):
+    app.config.from_object(config_options[config_name])
+
+    bootstap.init_app(app)
+
     return app
--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
