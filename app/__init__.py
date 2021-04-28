632a8faf25ff2efe4a78f9c4676d2bea7992c146
wangechimk <wangechik.michelle@gmail.com>
2021-04-28 22:37:24 +0300

    authentication of the app

--- app/__init__.py
+++ app/__init__.py
@@ -14,4 +14,7 @@ def create_app(config_name):
 
     bootstrap.init_app(app)
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
     return app
