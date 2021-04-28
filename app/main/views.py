01773eb159b727fdfe685b023aa63d944e0fdc4b
wangechimk <wangechik.michelle@gmail.com>
2021-04-28 22:27:46 +0300

    add navbar and index templates

---
+++ app/main/views.py
@@ -0,0 +1,13 @@
+from flask import render_template,url_for
+from . import main
+
+
+@main.route('/')
+def index():
+    """
+  View root page function that returns the index page and its data
+  """
+
+    title = 'Pomodoro'
+
+    return render_template('index.html', title=title)
