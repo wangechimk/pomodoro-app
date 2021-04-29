6ce9755a2597f47474f8c1c8b1ddaa5d16d56639
wangechimk <wangechik.michelle@gmail.com>
2021-04-29 21:05:51 +0300

    create a registration form

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, flash, request
 from . import auth
 from flask_login import login_user, logout_user, login_required
 from ..models import User
-from .forms import LoginForm
+from .forms import LoginForm,RegistrationForm
 
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -28,6 +28,14 @@ def logout():
     return redirect(url_for("main.index"))
 
 
-@auth.route('/register')
+@auth.route('/register',methods = ["GET","POST"])
 def register():
-    pass
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
+
