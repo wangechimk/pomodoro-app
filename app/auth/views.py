d744097c812c8db35c5f71d15cb82f09b4268d25
wangechimk <wangechik.michelle@gmail.com>
2021-04-29 20:57:45 +0300

    create login form

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,28 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash, request
 from . import auth
+from flask_login import login_user, logout_user, login_required
+from ..models import User
+from .forms import LoginForm
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return
-    render_template('auth/login.html')
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email=login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+        title = "Pomodoro login"
+
+    return render_template('auth/login.html', login_form=login_form, title=title)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been successfully logged out')
+    redirect(url_for(main.index))
